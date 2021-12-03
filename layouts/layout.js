27c514073dfaa620f9c0a4c43c993940a09bdb73
Kamui Huang <kamui.huang@ringcentral.com>
2021-12-04 00:54:15 +0800

    update

--- layouts/layout.js
+++ layouts/layout.js
@@ -42,7 +42,7 @@ const Layout = ({
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src="/favicon.jpg"
+                  src="/images/avatar.jpg"
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
