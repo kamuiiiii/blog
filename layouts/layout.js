67df984471bb0289bce43046abb4d14cb8fb028e
Kamui Huang <kamui.huang@ringcentral.com>
2021-12-04 21:43:03 +0800

    update

--- layouts/layout.js
+++ layouts/layout.js
@@ -35,8 +35,8 @@ const Layout = ({
           {frontMatter.title}
         </h1>
         {frontMatter.type[0] !== 'Page' && (
-          <nav className="flex mt-7 items-start text-gray-500 dark:text-gray-400">
-            <div className="flex mb-4">
+          <nav className="flex mt-5 mb-8 items-start text-gray-500 dark:text-gray-400 border-b border-gray-200">
+            <div className="flex">
               <a href={BLOG.socialLink || '#'} className="flex">
                 <Image
                   alt={BLOG.author}
@@ -49,7 +49,7 @@ const Layout = ({
               </a>
               <span className="block">&nbsp;/&nbsp;</span>
             </div>
-            <div className="mr-2 mb-4 md:ml-0">
+            <div className="mr-2 md:ml-0">
               {formatDate(
                 frontMatter?.date?.start_date || frontMatter.createdTime,
                 BLOG.lang
