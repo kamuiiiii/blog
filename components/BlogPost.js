a8f395435e690eb32c64190359e4f12dfaa4a5ed
Kamui Huang <kamui.huang@ringcentral.com>
2021-12-07 01:20:13 +0800

    update

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -6,9 +6,9 @@ const BlogPost = ({ post }) => {
   return (
     <Link href={`${BLOG.path}/${post.slug}`}>
       <a>
-        <article key={post.id} className="mb-6 md:mb-8">
+        <article key={post.id} className="mb-6 md:mb-12">
           <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-            <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
+            <h2 className="text-lg mb-2 cursor-pointer text-black dark:text-gray-100">
               {post.title}
             </h2>
             <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
@@ -16,7 +16,7 @@ const BlogPost = ({ post }) => {
             </time>
           </header>
           <main>
-            <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+            <p className="hidden text-sm md:block leading-8 text-gray-700 dark:text-gray-300">
               {post.summary}
             </p>
           </main>
